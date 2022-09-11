c5cb856be0e9ee0b696b72f3fbe7eb3e189e85e3
sonia-goyal <sonia.goyal@iitj.ac.in>
2022-09-11 11:18:03 +0530

    Hyper paramter tuning

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -13,11 +13,20 @@ hand-written digits, from 0-9.
 
 # Standard scientific Python imports
 import matplotlib.pyplot as plt
+import itertools
 
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 
+# Model hyper-parameters
+gamma_ = [0.001, 0.005, 0.01, 0.02]
+C_ = [0.5, 1, 1.5, 2]
+
+# h_param_dict = [{'gamma': GAMMA, 'C': C} for GAMMA in gamma_ for C in C_]
+
+h_params = list(itertools.product(gamma_, C_))
+print(h_params)
 ###############################################################################
 # Digits dataset
 # --------------
@@ -54,51 +63,64 @@ for ax, image, label in zip(axes, digits.images, digits.target):
 # vector classifier on the train samples. The fitted classifier can
 # subsequently be used to predict the value of the digit for the samples
 # in the test subset.
+best_acc = 0.0
+curr_acc = 0.0
 
-# flatten the images
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
+for h_param in h_params:
+    # Create a classifier: a support vector classifier
+    # flatten the images
+    n_samples = len(digits.images)
+    data = digits.images.reshape((n_samples, -1))
 
-# Create a classifier: a support vector classifier
-clf = svm.SVC(gamma=0.001)
+    clf = svm.SVC(gamma=h_param[0], C=h_param[1])
 
-# Split data into 50% train and 50% test subsets
-X_train, X_test, y_train, y_test = train_test_split(
-    data, digits.target, test_size=0.5, shuffle=False
-)
 
-# Learn the digits on the train subset
-clf.fit(X_train, y_train)
+    # Split data into 50% train and 50% test subsets
+    X_train, X_test, y_train, y_test = train_test_split(
+        data, digits.target, test_size=0.5, shuffle=False
+    )
 
-# Predict the value of the digit on the test subset
-predicted = clf.predict(X_test)
+    # Learn the digits on the train subset
+    clf.fit(X_train, y_train)
 
-###############################################################################
-# Below we visualize the first 4 test samples and show their predicted
-# digit value in the title.
+    # Predict the value of the digit on the test subset
+    predicted = clf.predict(X_test)
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, predicted):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title(f"Prediction: {prediction}")
+    curr_acc = metrics.accuracy_score(y_test, predicted)
 
-###############################################################################
-# :func:`~sklearn.metrics.classification_report` builds a text report showing
-# the main classification metrics.
 
-print(
-    f"Classification report for classifier {clf}:\n"
-    f"{metrics.classification_report(y_test, predicted)}\n"
-)
+    ###############################################################################
+    # Below we visualize the first 4 test samples and show their predicted
+    # digit value in the title.
 
+    # _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+    # for ax, image, prediction in zip(axes, X_test, predicted):
+    #     ax.set_axis_off()
+    #     image = image.reshape(8, 8)
+    #     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
+    #     ax.set_title(f"Prediction: {prediction}")
+
+    ###############################################################################
+    # :func:`~sklearn.metrics.classification_report` builds a text report showing
+    # the main classification metrics.
+
+    if curr_acc > best_acc:
+        best_acc = curr_acc
+        best_clf = clf
+        report = metrics.classification_report(y_test, predicted)
+        best_h_param = h_param
+        print(
+            f"Classification report for classifier {clf}: with parameter gamme = {best_h_param}\n"
+            f"{curr_acc}\n")
+
+print(f"Classification report for classifier {best_clf}: with parameter = {best_h_param}\n "
+      f"{report}\n")
 ###############################################################################
 # We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
 # true digit values and the predicted digit values.
 
-disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-disp.figure_.suptitle("Confusion Matrix")
-print(f"Confusion matrix:\n{disp.confusion_matrix}")
-
-plt.show()
+# disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
+# disp.figure_.suptitle("Confusion Matrix")
+# print(f"Confusion matrix:\n{disp.confusion_matrix}")
+#
+# plt.show()
