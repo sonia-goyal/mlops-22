40c2958de233ccc8c3ef8217ced4da9c7965c5c2
sonia-goyal <goya.25@iitj.ac.in>
2022-10-23 09:59:43 +0530

    Compare svm and decisiontree model

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,15 +1,16 @@
 # examples of unit test cases
 
 def sum(x, y):
-    return x+y
+    return x + y
+
 
 def test_sum():
     x = 5
     y = 7
-    z = sum(x,y)
+    z = sum(x, y)
     expected_z = 12
     assert z == expected_z
 
-def test_equal():
-    assert 1==1
 
+def test_equal():
+    assert 1 == 1
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,9 +13,7 @@ def test_get_h_param_comb():
     gamma_list = [0.01, 0.005, 0.001, 0.0005, 0.0001]
     c_list = [0.1, 0.2, 0.5, 0.7, 1, 2, 5, 7, 10]
 
-    params = {}
-    params["gamma"] = gamma_list
-    params["C"] = c_list
+    params = {"gamma": gamma_list, "C": c_list}
     h_param_comb = get_all_h_param_comb(params)
 
     assert len(h_param_comb) == len(gamma_list) * len(c_list)
@@ -26,9 +24,7 @@ def helper_h_params():
     gamma_list = [0.01, 0.005]
     c_list = [0.1, 0.2]
 
-    params = {}
-    params["gamma"] = gamma_list
-    params["C"] = c_list
+    params = {"gamma": gamma_list, "C": c_list}
     h_param_comb = get_all_h_param_comb(params)
     return h_param_comb
 
