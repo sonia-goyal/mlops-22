6caaec2cfa2de3b585245db2c3af2d5298c0dca0
sonia-goyal <goya.25@iitj.ac.in>
2022-10-30 15:47:04 +0530

    Model comparison with decision tree Assignment 3

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # mlops-22
 Repo for mlops class July 2022
 
+Run SVM  Decision Tree
+1   0.97  0.88
+2   0.98  0.88
+3   0.98  0.81
+4   1.0   0.89
+5   0.99  0.83
+mean:  0.98   0.86
+std:  0.01   0.03
--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -1,9 +1,9 @@
-# PART: library dependencies -- sklear, torch, tensorflow, numpy, transformers
-
 # Import datasets, classifiers and performance metrics
+# from joblib import dump, load
+# import pdb
 from sklearn import datasets, svm, metrics
-import pdb
 from sklearn import tree
+import numpy as np
 
 from utils import (
     preprocess_digits,
@@ -11,12 +11,9 @@ from utils import (
     h_param_tuning,
     h_param_tuning_dec,
     data_viz,
-    pred_image_viz,
     get_all_h_param_comb,
-    tune_and_save,
     predict
 )
-from joblib import dump, load
 
 train_frac, dev_frac, test_frac = 0.8, 0.1, 0.1
 assert train_frac + dev_frac + test_frac == 1.0
@@ -29,62 +26,32 @@ params = {"gamma": gamma_list, "C": c_list}
 
 h_param_comb = get_all_h_param_comb(params)
 
-
 # PART: load dataset -- data from csv, tsv, jsonl, pickle
 digits = datasets.load_digits()
 data_viz(digits)
 data, label = preprocess_digits(digits)
-# housekeeping
 del digits
-
-#
-# x_train, y_train, x_dev, y_dev, x_test, y_test = train_dev_test_split(
-#     data, label, train_frac, dev_frac
-# )
-
-
-# def create_split():
-#     x_train, y_train, x_dev, y_dev, x_test, y_test = train_dev_test_split(
-#         data, label, train_frac, dev_frac
-#     )
-
-
-
 
 models = [[tree.DecisionTreeClassifier(), 'decision_tree'], [svm.SVC(), 'svm']]
 perf_test = {}
 metric = metrics.accuracy_score
 for model in models:
+    ls = []
     for i in range(5):
-        print(f"\nTraining for split: {i+1}")
+        print(f"\nTraining for split: {i + 1}")
         x_train, y_train, x_dev, y_dev, x_test, y_test = train_dev_test_split(data, label, train_frac, dev_frac)
         clf = model[0]
         if model[1] == 'svm':
-            best_model, best_metric, best_h_params = h_param_tuning(h_param_comb, clf, x_train, y_train, x_dev, y_dev, metric)
+            best_model, best_metric, best_h_params = h_param_tuning(h_param_comb, clf, x_train, y_train, x_dev, y_dev,
+                                                                    metric)
         else:
             best_model, best_metric, best_h_params = h_param_tuning_dec(clf, x_train, y_train, x_dev, y_dev, metric)
-        perf_test[model[1] + '_' + str(i)] = predict(best_model, x_test, y_test, metric)
+        ls.append(predict(best_model, x_test, y_test, metric))
+    perf_test[model[1]] = ls
 
+print("\n")
+for i in range(5):
+    print(i + 1, round(perf_test['svm'][i], 2), round(perf_test['decision_tree'][i], 2))
+print("mean: ", round(np.mean(perf_test['svm']), 2), " ", round(np.mean(perf_test['decision_tree']), 2))
+print("std: ", round(np.std(perf_test['svm']), 2), " ", round(np.std(perf_test['decision_tree']), 2))
 
-print(perf_test)
-
-# actual_model_path = tune_and_save(
-#     clf, x_train, y_train, x_dev, y_dev, metric, h_param_comb, model_path=None
-# )
-#
-#
-# # 2. load the best_model
-# best_model = load(actual_model_path)
-#
-# # PART: Get test set predictions
-# # Predict the value of the digit on the test subset
-# predicted = best_model.predict(x_test)
-#
-# pred_image_viz(x_test, predicted)
-
-# 4. report the test set accurancy with that best model.
-# PART: Compute evaluation metrics
-# print(
-#     f"Classification report for classifier {clf}:\n"
-#     f"{metrics.classification_report(y_test, predicted)}\n"
-# )
