022ed78dc8aa791c6bd041576fe06725736b8332
sonia-goyal <goya.25@iitj.ac.in>
2022-11-13 16:03:49 +0530

    create an app to predict number from image

--- api/app.py
+++ api/app.py
@@ -1,7 +1,29 @@
 from flask import Flask
+from flask import jsonify
+from flask import request
+from joblib import dump, load
 
 app = Flask(__name__)
+model_path = "/Users/soniagoyal/mlops/mlops-22/svm_gamma=0.001_C=0.5.joblib"
+model = load(model_path)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/sum", methods=["POST"])
+def sum():
+    x = request.json['x']
+    y = request.json['y']
+    z = x + y
+    return {'sum': z}
+
+
+@app.route("/predict", methods=['POST'])
+def predict_digit():
+    image = request.json['image']
+    print("done loading")
+    predicted = model.predict([image])
+    return {"y_predicted": int(predicted[0])}
+
