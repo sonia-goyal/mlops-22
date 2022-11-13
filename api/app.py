ea8421fda243a534f4e16d477b5790127a682367
sonia-goyal <goya.25@iitj.ac.in>
2022-11-13 21:16:00 +0530

    Containerize the flask application to compare two images

--- api/app.py
+++ api/app.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
-from joblib import dump, load
+from joblib import load
 
 app = Flask(__name__)
-model_path = "/Users/soniagoyal/mlops/mlops-22/svm_gamma=0.001_C=0.5.joblib"
+model_path = "svm_gamma=0.001_C=0.5.joblib"
 model = load(model_path)
+
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<b><p>Hello, World!</p><b>"
 
 
 @app.route("/sum", methods=["POST"])
@@ -22,8 +23,14 @@ def sum():
 
 @app.route("/predict", methods=['POST'])
 def predict_digit():
-    image = request.json['image']
+    image1 = request.json['image1']
+    image2 = request.json['image2']
     print("done loading")
-    predicted = model.predict([image])
-    return {"y_predicted": int(predicted[0])}
+    predicted1 = model.predict([image1])
+    predicted2 = model.predict([image2])
+    if int(predicted1[0]) == int(predicted2[0]):
+        val = 'Same number'
+    else:
+        val = 'Different numbers'
+    return {"prediction": val}
 
